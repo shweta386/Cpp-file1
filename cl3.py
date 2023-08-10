d45d4e2c75ab1ce0344b6ee91865b30fac0cf5c2
shweta386 <114805286+shweta386@users.noreply.github.com>
2023-08-10 08:48:19 +0530

    Add files via upload

---
+++ cl3.py
@@ -0,0 +1,17 @@
+class Demo():
+	a=10
+	def read(self):
+		self.a=int(input("Enter value for a : "))
+	def disp(self):
+		print("In disp()...")
+		print(self.a)
+	def setValue(self,b):
+		self.a=b
+
+
+d1=Demo()
+d1.read()
+d1.disp()
+
+d1.setValue(200)
+d1.disp()
